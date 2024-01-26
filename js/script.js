d64ef39202be128bce56468c9f19060db02348c7
priyanka shaw <priyankashaw680@gmail.com>
2024-01-26 16:39:33 +0530

    making changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,51 @@
-function imageHandler(){
-   
-    const promice = fetch('https://api.unsplash.com/search/photos?page=1&query=dog&client_id=9xDjV6ba5gKToWK3rjaq9CfNmj7v9L9SGwocDxmLRqM');
+
+function imageHandler(event, search = true){
+    event.preventDefault();
+   const searchValue = document.querySelector("#inputbox").value;
+   console.log(searchValue);
+    const promice = fetch(`https://api.unsplash.com/search/photos?page=${count}&query=${searchValue}&client_id=9xDjV6ba5gKToWK3rjaq9CfNmj7v9L9SGwocDxmLRqM`);
     console.log(promice);
     promice.then((response)=>{
-       console.log(response);
+       //console.log(response);
        return response.json();
     })
     .then((result)=>{
-        console.log(result.results[0].slug);
-        console.log(result.results[0]);
-        const output = result.results[0].slug;
-        document.querySelector("div").append(output);
+        const array = result.results;
+        const innerContainer = document.querySelector(".innerContainer");
+        if(search==true){
+            innerContainer.innerHTML = "";
+        }
+       
+        array.forEach((element) => {
+            console.log(element.urls.small);
+            const url = element.urls.small;
+            const description = element.alt_description;
+            console.log(description)
+           
+            innerContainer.innerHTML += `<div class="imageContainer">
+            <figure>
+                <img src="${url}" alt="image">
+            </figure>
+            <figcaption>${description}</figcaption>
+        </div>`;
+        });
     })
-    // const promice = fetch('https://dog.ceo/api/breeds/image/random');
-    // console.log(promice);
-    // promice.then((response)=>{
-    //     //console.log(response.json());
-    //     return response.json();
-    // })
-    // .then((result)=>{
-    //     console.log(result);
-    // })
-    // .catch((error)=>{
-    //     console.log("getting error");
-    // })
+    .catch((error) =>{
+        console.log("not gettinhg file");
+    })
+
 
 }
-document.querySelector("div");
-imageHandler();
+const  form = document.querySelector("form");
+form.addEventListener("submit" , imageHandler);
+console.log(form);
+// document.querySelector("from").addEventListener("submit" , (e)=>{
+//     e.preventDefault();
+//     imageHandler();
+// });
+var count = 1;
+document.querySelector("#btn").addEventListener("click" , (e)=>{
+    count++;
+    imageHandler(e , false);
+    
+})
